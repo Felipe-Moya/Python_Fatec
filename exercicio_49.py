f767f3e33ff081575aebe4821521500ee274bd51
Felipe Moya Figueiredo <124535653+Felipe-Moya@users.noreply.github.com>
2023-10-28 11:10:10 -0300

    Números impares
    Faz o mesmo que o exercício 48, porem pede que o número fornecido seja maior que 0.

---
+++ exercicio_49.py
@@ -0,0 +1,11 @@
+while True:
+    i = int(input("Digite um numero: "))
+    c = 0
+    if i >0:
+        break
+    else:
+        print("O número deve ser maior que 0.")
+while i >= c:
+    if c % 2 == 1:
+        print(c)
+    c += 1
